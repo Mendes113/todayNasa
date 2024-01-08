604f8390940fd540495ddca117c0cc0984af2098
Mendes <andremendes0113@gmail.com>
2024-01-08 19:02:08 -0300

    package

---
+++ bot/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (	
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+}
+
